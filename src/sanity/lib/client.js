1367a20257ad30605c73d86ed14c91a1d5e33a69
jcbl india marketing <jcblindiamarketing@gmail.com>
2026-09-02 11:24:03 +0530

    Disable Sanity CDN caching

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 console.log("ENV CHECK:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID)
